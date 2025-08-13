ddd8a47be9ffd2ffcb906840521ef13de047ad93
erfilho <erineldx@gmail.com>
2025-08-13 15:41:53 -0300

    feat: added an reusable button with color change

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
+<script setup>
+import Buttons from "./components/Buttons.vue";
+</script>
+
 <template>
   <div class="w-full min-h-dvh flex flex-col items-center justify-center">
-    <p class="">My Workflow First commit </p>
+    <p class="font-semibold">My Workflow First commit</p>
+    <div class="flex flex-row w-1/3 justify-around items-center p-4">
+      <Buttons type="primary" title="Click me" />
+    </div>
   </div>
 </template>
