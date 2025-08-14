214c45b7eb5ed42135daccd749105c395fff2246
erfilho <erineldx@gmail.com>
2025-08-14 14:09:33 -0300

    feat: added a navbart with button for toggle with links and view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import CardInfo from "./components/CardInfo.vue";
+  import Navbar from "./components/Navbar.vue"
 </script>
 
 <template>
-  <div class="w-full min-h-dvh flex flex-col items-center justify-center">
-    <p class="font-semibold">My Workflow First commit</p>
-    <div class="flex flex-row w-1/3 justify-around items-center p-4">
-      <CardInfo title="Teste" description="lorem ipsum dolom sit amet" />
-    </div>
-  </div>
+  <Navbar />
+  <router-view />
 </template>
